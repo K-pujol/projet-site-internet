dcfc0cd3731558ccab8d60787f3a2843c0bc9b69
K-pujol <killian.pujol@le-campus-numerique.fr>
2025-04-29 11:46:06 +0200

    modification JS pour "automatiser" avec une boucle la création des ccartes

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 const filename = window.location.pathname.split('/').pop();
 document.getElementById('page-title').textContent = filename || 'Accueil';
 
+
+
+// loop articles : article (title, content, picture)
+const containeurCarte = document.getElementById('cards');
+
+const nbCarte = 5; // Choisir combien de cartes créer
+
+for (let i = 0; i < nbCarte; i++) {
+    const carte = document.createElement('article');
+    carte.classList.add('card'); // Ajoute une classe "card" à l'élément créé
+
+    const headerCarte = document.createElement('header');
+    const titreCarte = document.createElement('h2');
+    titreCarte.textContent = `Titre film ${i + 1}`;  
+    headerCarte.appendChild(titreCarte); // Remplacer cardTitle par titreCarte
+
+
+    const imageCarte = document.createElement('img');
+    imageCarte.src = './photo/chien.jpg';  
+    imageCarte.alt = `Affiche du film ${i + 1}`;
+
+
+    const contenuCarte = document.createElement('div');
+    contenuCarte.classList.add('content');
+
+    const resumeCarte = document.createElement('p');
+    resumeCarte.textContent = 'Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus...'; 
+    contenuCarte.appendChild(resumeCarte);
+
+
+    carte.appendChild(headerCarte);
+    carte.appendChild(imageCarte);
+    carte.appendChild(contenuCarte);
+
+    containeurCarte.appendChild(carte);
+
+}
+
+
+
+// end_loop
