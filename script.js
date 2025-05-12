32a7ae986211c80dc1ec49db3e40e8037bb2e8a7
K-pujol <killian.pujol@le-campus-numerique.fr>
2025-05-12 13:33:45 +0200

    ajout galerie + formulaire pour galerie

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ const options = {
 };
 
 //--------------------------------Appel API-----------------------------\\
+
+let conteneur = document.getElementById('cards-api');
+let mode = "complet";
+let nbAffiche = 16;
+if (!conteneur) {
+    conteneur = document.getElementById('cards-images');
+    mode = "images";
+}
+
+
 fetch(`https://api.themoviedb.org/3/movie/top_rated?language=fr-FR&page=${pageAleatoire}`, options)
     .then(res => res.json())
     .then(data => {
         const films = data.results;
-        const conteneur = document.getElementById('cards-api');
-        afficherCartesFilms(films, 16, conteneur);
+        afficherCartesFilms(films, nbAffiche, conteneur, mode);
     })
 
 
@@ -24,8 +33,7 @@ console.log(`Vous êtes page: ${pageAleatoire}`);
 
 
 
-function afficherCartesFilms(films, nbCarte, conteneurCarte) {
-    // pour limiter si on a moins de films que ce qu'on en demande
+function afficherCartesFilms(films, nbCarte, conteneurCarte, mode = "complet") {
     const max = Math.min(nbCarte, films.length);
 
     for (let i = 0; i < max; i++) {
@@ -35,42 +43,45 @@ function afficherCartesFilms(films, nbCarte, conteneurCarte) {
         const image = 'https://image.tmdb.org/t/p/w500' + film.poster_path;
         const dateSortie = film.release_date;
 
-
         const carte = document.createElement('article');
         carte.classList.add('card');
+
 
         const headerCarte = document.createElement('header');
         const titreCarte = document.createElement('h2');
         titreCarte.textContent = titre;
         headerCarte.appendChild(titreCarte);
+        carte.appendChild(headerCarte);
 
-        const footerCarte = document.createElement('footer');
-        const sortieCarte = document.createElement('h2');
-        sortieCarte.textContent = dateSortie;
-        footerCarte.appendChild(sortieCarte);
 
 
         const imageCarte = document.createElement('img');
         imageCarte.src = image;
         imageCarte.alt = `Affiche du film ${titre}`;
-
-        const contenuCarte = document.createElement('div');
-        contenuCarte.classList.add('content');
-
-        const resumeCarte = document.createElement('p');
-        resumeCarte.textContent = description;
-        contenuCarte.appendChild(resumeCarte);
-
-
-        carte.appendChild(headerCarte);
         carte.appendChild(imageCarte);
-        carte.appendChild(contenuCarte);
-        carte.appendChild(footerCarte);
 
+
+        if (mode === "complet") {
+            const contenuCarte = document.createElement('div');
+            contenuCarte.classList.add('content');
+
+            const resumeCarte = document.createElement('p');
+            resumeCarte.textContent = description;
+            contenuCarte.appendChild(resumeCarte);
+            carte.appendChild(contenuCarte);
+
+            const footerCarte = document.createElement('footer');
+            const sortieCarte = document.createElement('h2');
+            sortieCarte.textContent = dateSortie;
+            footerCarte.appendChild(sortieCarte);
+            carte.appendChild(footerCarte);
+        }
 
         conteneurCarte.appendChild(carte);
     }
 }
+
+
 
 const boutonRandom = document.querySelector('.button');
 
@@ -87,7 +98,7 @@ function FilmRandom() {
             const films = data.results;
             const conteneur = document.getElementById('cards-api');
             conteneur.innerHTML = ''; // on vide avant d'ajouter les nouvelles cartes
-            afficherCartesFilms(films, 20, conteneur);
+            afficherCartesFilms(films, nbAffiche, conteneur);
         })
         .catch(err => console.error(err));
 }
@@ -130,75 +141,143 @@ overlay.addEventListener("click", (e) => {
 
 const filmAjouter = [];
 
-document.querySelector(".form").addEventListener("submit", function (event) {
-    event.preventDefault();
+const formFilm = document.querySelector("#form-film");
+if (formFilm) {
+    formFilm.addEventListener("submit", function (event) {
+        event.preventDefault();
 
-    const filmTitle = document.getElementById("tfilm").value;
-    const urlImg = document.getElementById("poster").value;
-    const fichierImg = document.getElementById("posterFile").files[0];
-    const filmSyno = document.getElementById("syno").value;
-    const filmAnnee = document.getElementById("annee").value;
+        const filmTitle = document.getElementById("tfilm").value;
+        const urlImg = document.getElementById("poster").value;
+        const fichierImg = document.getElementById("posterFile").files[0];
+        const filmSyno = document.getElementById("syno").value;
+        const filmAnnee = document.getElementById("annee").value;
 
-    // pour vérifier qu'au moins un des champs pour l'image est utilisé
-    if (!urlImg && !fichierImg) {
-        alert("Veuillez fournir un lien vers une image ou importer un fichier.");
-        return; 
+        // pour vérifier qu'au moins un des champs pour l'image est utilisé
+        if (!urlImg && !fichierImg) {
+            alert("Veuillez fournir un lien vers une image ou importer un fichier.");
+            return;
+        }
+
+        let filmImg;
+        if (fichierImg) {
+            filmImg = URL.createObjectURL(fichierImg);
+        } else {
+            filmImg = urlImg;
+        }
+
+        creerCartePerso(filmTitle, filmSyno, filmImg, filmAnnee);
+
+        formContainer.style.display = "none";
+        overlay.style.display = "none";
+
+        // Nettoyage des champs
+        document.getElementById("tfilm").value = "";
+        document.getElementById("poster").value = "";
+        document.getElementById("posterFile").value = "";
+        document.getElementById("syno").value = "";
+        document.getElementById("annee").value = "";
+    });
+
+
+
+    function creerCartePerso(titre, synopsis, image, annee) {
+
+        const carte = document.createElement('article');
+        carte.classList.add('card');
+
+        const headerCarte = document.createElement('header');
+        const titreCarte = document.createElement('h2');
+        titreCarte.textContent = titre;
+        headerCarte.appendChild(titreCarte);
+
+        const imageCarte = document.createElement('img');
+        imageCarte.src = image;
+        imageCarte.alt = `Affiche du film ${titre}`;
+
+        const contenuCarte = document.createElement('div');
+        contenuCarte.classList.add('content');
+
+        const resumeCarte = document.createElement('p');
+        resumeCarte.textContent = synopsis;
+        contenuCarte.appendChild(resumeCarte);
+
+        const footerCarte = document.createElement('footer');
+        const sortieCarte = document.createElement('h2');
+        sortieCarte.textContent = annee;
+        footerCarte.appendChild(sortieCarte);
+
+        carte.appendChild(headerCarte);
+        carte.appendChild(imageCarte);
+        carte.appendChild(contenuCarte);
+        carte.appendChild(footerCarte);
+
+        document.getElementById("cards-persos").appendChild(carte);
     }
-
-    let filmImg;
-    if (fichierImg) {
-        filmImg = URL.createObjectURL(fichierImg);
-    } else {
-        filmImg = urlImg;
-    }
-
-    creerCartePerso(filmTitle, filmSyno, filmImg, filmAnnee);
-
-    formContainer.style.display = "none";
-    overlay.style.display = "none";
-
-    // Nettoyage des champs
-    document.getElementById("tfilm").value = "";
-    document.getElementById("poster").value = "";
-    document.getElementById("posterFile").value = "";
-    document.getElementById("syno").value = "";
-    document.getElementById("annee").value = "";
-});
-
-
-
-function creerCartePerso(titre, synopsis, image, annee) {
-
-    const carte = document.createElement('article');
-    carte.classList.add('card');
-
-    const headerCarte = document.createElement('header');
-    const titreCarte = document.createElement('h2');
-    titreCarte.textContent = titre;
-    headerCarte.appendChild(titreCarte);
-
-    const imageCarte = document.createElement('img');
-    imageCarte.src = image;
-    imageCarte.alt = `Affiche du film ${titre}`;
-
-    const contenuCarte = document.createElement('div');
-    contenuCarte.classList.add('content');
-
-    const resumeCarte = document.createElement('p');
-    resumeCarte.textContent = synopsis;
-    contenuCarte.appendChild(resumeCarte);
-
-    const footerCarte = document.createElement('footer');
-    const sortieCarte = document.createElement('h2');
-    sortieCarte.textContent = annee;
-    footerCarte.appendChild(sortieCarte);
-
-    carte.appendChild(headerCarte);
-    carte.appendChild(imageCarte);
-    carte.appendChild(contenuCarte);
-    carte.appendChild(footerCarte);
-
-    document.getElementById("cards-persos").appendChild(carte);
 }
 
+//-----------------------------------GALERIE-----------------------------------\\
 
+const imageAjouter = [];
+
+const formImage = document.querySelector("#form-galerie");
+if (formImage) {
+    formImage.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+
+        const nomImg = document.getElementById("tImage").value;
+        const urlImg = document.getElementById("poster").value;
+        const fichierImg = document.getElementById("posterFile").files[0];
+
+
+        if (!nomImg) {
+            alert("Veuillez entrer un nom pour l'image.");
+            return;
+        }
+
+        // pour vérifier qu'au moins un des champs pour l'image est utilisé
+        if (!urlImg && !fichierImg) {
+            alert("Veuillez fournir un lien vers une image ou importer un fichier.");
+            return;
+        }
+
+        let Img;
+        if (fichierImg) {
+            Img = URL.createObjectURL(fichierImg);
+        } else {
+            Img = urlImg;
+        }
+
+        ajoutImagePerso(nomImg, Img);
+
+        formContainer.style.display = "none";
+        overlay.style.display = "none";
+
+        // Nettoyage des champs
+        document.getElementById("tImage").value = "";
+        document.getElementById("poster").value = "";
+        document.getElementById("posterFile").value = "";
+    });
+
+
+
+    function ajoutImagePerso(titre, image) {
+
+        const carte = document.createElement('article');
+        carte.classList.add('card');
+
+        const headerCarte = document.createElement('header');
+        const titreCarte = document.createElement('h2');
+        titreCarte.textContent = titre;
+        headerCarte.appendChild(titreCarte);
+
+        const imageCarte = document.createElement('img');
+        imageCarte.src = image;
+        imageCarte.alt = `Image ${titre}`;
+
+        carte.appendChild(headerCarte);
+        carte.appendChild(imageCarte);
+
+        document.getElementById("cards-images-perso").appendChild(carte);
+    }
+}
